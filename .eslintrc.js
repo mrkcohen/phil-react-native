bbd494d5d7f2272f350f2802d3ac09a2a5e54cb6
Mike Cohen <mrkcohen@gmail.com>
2020-04-02 09:23:35 -0700

    Setup lint for project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  plugins: ['prettier'],
+  extends: [
+      'eslint:recommended',
+      '@react-native-community',
+      'prettier'
+  ],
+  rules: {
+      'prettier/prettier': 'error',
+      'no-extra-semi': 'off',
+      'semi': 0
+  },
+  env: {
+      'es6': true,
+
+  }
 };
